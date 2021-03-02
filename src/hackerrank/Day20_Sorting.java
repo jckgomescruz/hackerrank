87fa6ee82ab29d3cdb5be4329b9e0cad4c797dc5
Joao Cruz <jckgomescruz@gmail.com>
2021-03-02 17:00:37 -0300

    30 days of Code: Day 20 - Sorting

---
+++ src/hackerrank/Day20_Sorting.java
@@ -0,0 +1,46 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Day20_Sorting {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        
+        int totalSwaps = 0;
+        
+        // Bubble Sort
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+            
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                	int number = a[j];
+                	a[j] = a[j + 1]; 
+                	a[j + 1] = number;
+                    numberOfSwaps++;
+                    totalSwaps++;
+                }
+            }
+            
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+        
+        in.close();
+    }
+}
